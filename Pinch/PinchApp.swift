cdf30a6cf59b2fff97b803949aed33c8836470f8
Firdavs Bagirov <firdavsbagirov@abdigital.uz>
2022-01-14 16:00:35 +0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Firdavs Bagirov on 14/01/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
